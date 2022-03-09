c496fea77ffa7a40a705e6e4e7a712f13f99cfce
czj <caizhenjie@eastwestec.com>
2022-03-09 10:57:22 +0800

    FINISH：mock接口完成

--- frontend/mock/index.ts
+++ frontend/mock/index.ts
@@ -7,4 +7,7 @@ console.log('handlers', handlers);
 
 // This configures a request mocking server with the given request handlers.
 export const worker = setupWorker(...handlers)
-worker.start()
+// worker.start({
+//     // 对于没有 mock 的接口直接通过，避免异常
+//     onUnhandledRequest: 'bypass',
+// })
